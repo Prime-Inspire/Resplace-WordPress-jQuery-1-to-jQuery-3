a1f223f4762b9510caa53eed9693361ccdb2bf30
ZINFOMATIC <60377115+zinfomatic@users.noreply.github.com>
2020-08-23 12:04:05 +0530

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,9 @@
+// Replace jQuery in WP
+// Add this script in the top most of functions.php file
+
+function replace_core_jquery_version() {
+    wp_deregister_script( 'jquery' );
+    // Change the URL if you want to load a local copy of jQuery from your own server.
+    wp_register_script( 'jquery', "https://code.jquery.com/jquery-3.1.1.min.js", array(), '3.1.1' );
+}
+add_action( 'wp_enqueue_scripts', 'replace_core_jquery_version' );
